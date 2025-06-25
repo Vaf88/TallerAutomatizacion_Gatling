563d7f2901daf5d35e9d067f055a5125c1d96e73
Vaf88 <95873303+Vaf88@users.noreply.github.com>
2025-06-24 21:12:52 -0500

    Update Data.scala

--- src/test/scala/Demo/Data.scala
+++ src/test/scala/Demo/Data.scala
@@ -2,6 +2,6 @@ package Demo
 
 object Data{
     val url= "https://chat.google.com/room/AAAAen-jOps/kV8JIXW1ytw/kV8JIXW1ytw?cls=10"
-    val username = "valeria@gmail.com"
+    val email = "valeria@gmail.com"
     val password = "Negro88Teamo"
 }
